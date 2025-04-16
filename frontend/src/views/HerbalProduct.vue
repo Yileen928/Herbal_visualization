5585175355345874f202fb767226a4b2dff45720
yongh27 <yongh9129@gmail.com>
2025-04-16 17:54:04 +0800

    添加中国地图JSON数据

--- frontend/src/views/HerbalProduct.vue
+++ frontend/src/views/HerbalProduct.vue
@@ -3,10 +3,15 @@
     <div class="herbal-product-background">
       <div class="herbal-product-container1">
         <div class="herbal-product-chart1"></div>
-        <div class="herbal-product-chart2">  </div>   
+        <div class="herbal-product-chart2"></div>
       </div>
       <div class="herbal-product-container2">
-        <div class="herbal-product-chart3"></div>
+        <div class="herbal-product-chart3" ref="yunnanMap" style="position: relative;">
+          <div id="map-container" ref="mapContainer" style="width: 100%; height: 500px;"></div>
+          <div class="map-footer" style="position: absolute; bottom: 10px; left: 0; right: 0; text-align: center;">
+            <span>数据来源：数字云药平台、中医百科网站</span>
+          </div>
+        </div>
         <div class="herbal-product-chart2"></div>
       </div>
       <div class="herbal-product-container3">
@@ -18,12 +23,108 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import * as echarts from 'echarts';
+import yunnanJson from '../assets/yunnan.json';
 
-const router = useRouter()
-const goToHci = () => {
-  router.push('/hci')
-}
+const router = useRouter();
+const mapContainer = ref(null);
+
+// 城市数据
+const cityData = {
+  '昆明市': { plantingArea: 16.1, agriculturalYield: 5.0, agriculturalValue: 30.9, processingOutput: 11.0, processingValue: 158.9, medicineCount: 38 },
+  '曲靖市': { plantingArea: 90.1, agriculturalYield: 26.8, agriculturalValue: 82.7, processingOutput: 7.2, processingValue: 29.1, medicineCount: 35 },
+  '玉溪市': { plantingArea: 9.3, agriculturalYield: 6.5, agriculturalValue: 4.6, processingOutput: 1.4, processingValue: 13.7, medicineCount: 19 },
+  '保山市': { plantingArea: 51.6, agriculturalYield: 5.8, agriculturalValue: 39.3, processingOutput: 5.3, processingValue: 51.3, medicineCount: 29 },
+  '昭通市': { plantingArea: 46.6, agriculturalYield: 5.2, agriculturalValue: 54.7, processingOutput: 1.1, processingValue: 36.4, medicineCount: 22 },
+  '丽江市': { plantingArea: 31.3, agriculturalYield: 4.8, agriculturalValue: 17.7, processingOutput: 5.0, processingValue: 21.2, medicineCount: 38 },
+  '普洱市': { plantingArea: 48.0, agriculturalYield: 9.7, agriculturalValue: 42.7, processingOutput: 2.5, processingValue: 13.8, medicineCount: 36 },
+  '临沧市': { plantingArea: 69.0, agriculturalYield: 3.1, agriculturalValue: 11.7, processingOutput: 0.1, processingValue: 1.2, medicineCount: 18 },
+  '德宏傣族景颇族自治州': { plantingArea: 26.6, agriculturalYield: 2.5, agriculturalValue: 13.3, processingOutput: 0.2, processingValue: 1.8, medicineCount: 12 },
+  '怒江傈僳族自治州': { plantingArea: 127.1, agriculturalYield: 2.2, agriculturalValue: 6.6, processingOutput: 1.8, processingValue: 5.5, medicineCount: 6 },
+  '迪庆藏族自治州': { plantingArea: 14.2, agriculturalYield: 2.5, agriculturalValue: 7.0, processingOutput: 0.3, processingValue: 0.8, medicineCount: 3 },
+  '大理白族自治州': { plantingArea: 38.3, agriculturalYield: 4.8, agriculturalValue: 38.7, processingOutput: 2.8, processingValue: 41.2, medicineCount: 9 },
+  '楚雄彝族自治州': { plantingArea: 44.0, agriculturalYield: 13.8, agriculturalValue: 37.0, processingOutput: 3.3, processingValue: 15.8, medicineCount: 6 },
+  '红河哈尼族彝族自治州': { plantingArea: 160.9, agriculturalYield: 20.4, agriculturalValue: 51.4, processingOutput: 11.3, processingValue: 33.0, medicineCount: 15 },
+  '文山壮族苗族自治州': { plantingArea: 229.5, agriculturalYield: 30.1, agriculturalValue: 88.8, processingOutput: 10.9, processingValue: 51.6, medicineCount: 16 },
+  '西双版纳傣族自治州': { plantingArea: 28.0, agriculturalYield: 1.2, agriculturalValue: 8.4, processingOutput: 2.9, processingValue: 13.3, medicineCount: 23 }
+};
+
+// 处理地图点击事件
+const handleMapClick = (params) => {
+  if (params.data) {
+    router.push('/herbal-product');
+  }
+};
+
+onMounted(() => {
+  const chart = echarts.init(mapContainer.value);
+  echarts.registerMap('yunnan', yunnanJson);
+
+  const option = {
+    title: {
+      text: '云南省中药材地理分布',
+      left: 'center',
+      textStyle: {
+        fontSize: 24,
+        color: '#333'
+      }
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: (params) => {
+        const cityInfo = cityData[params.name];
+        if (cityInfo) {
+          return `${params.name}<br/>
+            中药材种植面积：${cityInfo.plantingArea} 万亩<br/>
+            农业产量：${cityInfo.agriculturalYield} 万吨<br/>
+            农业产值：${cityInfo.agriculturalValue} 亿元<br/>
+            加工产量：${cityInfo.processingOutput} 万吨<br/>
+            加工产值：${cityInfo.processingValue} 亿元<br/>
+            药材数量：${cityInfo.medicineCount} 种`;
+        }
+        return params.name; // 如果没有找到数据，返回城市名称
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 230, // 根据数据调整
+      left: 'left',
+      top: 'bottom',
+      text: ['高', '低'],
+      calculable: true,
+      inRange: {
+        color: ['#e0f7e0', '#1a6d1a']
+      }
+    },
+    series: [{
+      name: '中药材品种',
+      type: 'map',
+      map: 'yunnan', // 使用云南省的地图
+      roam: true,
+      emphasis: {
+        label: {
+          show: true
+        }
+      },
+      data: Object.entries(cityData).map(([name, data]) => ({
+        name,
+        value: data.medicineCount // 以药材数量为值
+      }))
+    }]
+  };
+
+  chart.setOption(option);
+
+  // 监听点击事件
+  chart.on('click', handleMapClick);
+
+  // 监听窗口大小变化
+  window.addEventListener('resize', () => {
+    chart.resize();
+  });
+});
 </script>
 
 <style scoped>
--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -8,7 +8,7 @@
 import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
 import * as echarts from 'echarts'
-import chinaJson from '../assets/china.json'
+import chinaJson from '../assets/China2.json'
 
 const router = useRouter()
 const mapContainer = ref(null)
@@ -51,6 +51,15 @@ onMounted(() => {
       name: '中药材品种',
       type: 'map',
       map: 'china',
+      regions: [{
+      name: '南海诸岛',
+      itemStyle: {
+        opacity: 0 // 完全透明
+      },
+      label: {
+        show: false // 隐藏标签
+      }
+    }],
       roam: true,
       emphasis: {
         label: {
@@ -58,13 +67,40 @@ onMounted(() => {
         }
       },
       data: [
-        {name: '云南省', value: 95},
-        {name: '四川省', value: 88},
-        {name: '贵州省', value: 82},
-        {name: '广西壮族自治区', value: 78},
-        {name: '湖南省', value: 75},
-        {name: '陕西省', value: 70},
-        {name: '甘肃省', value: 68}
+        {name: '北京市', value: 4},
+        {name: '天津市', value: 4},
+        {name: '上海市', value: 5},
+        {name: '重庆市', value: 7},
+        {name: '河北省', value: 21},
+        {name: '山西省', value: 19},
+        {name: '辽宁省', value: 18},
+        {name: '吉林省', value: 11},
+        {name: '黑龙江省', value: 6},
+        {name: '江苏省', value: 26},
+        {name: '浙江省', value: 37},
+        {name: '安徽省', value: 29},
+        {name: '福建省', value: 41},
+        {name: '江西省', value: 35},
+        {name: '山东省', value: 18},
+        {name: '河南省', value: 26},
+        {name: '广东省', value: 47},
+        {name: '湖南省', value: 37},
+        {name: '湖北省', value: 42},
+        {name: '海南省', value: 19},
+        {name: '四川省', value: 75},
+        {name: '贵州省', value: 47},
+        {name: '云南省', value: 156},
+        {name: '陕西省', value: 33},
+        {name: '甘肃省', value: 20},
+        {name: '青海省', value: 5},
+        {name: '台湾省', value: 16},
+        {name: '内蒙古自治区', value: 10},
+        {name: '广西壮族自治区', value: 62},
+        {name: '西藏自治区', value: 18},
+        {name: '宁夏回族自治区', value: 2},
+        {name: '新疆维吾尔自治区', value: 4},
+        {name: '香港特别行政区', value: 0},
+        {name: '澳门特别行政区', value: 0}
       ]
     }]
   }
