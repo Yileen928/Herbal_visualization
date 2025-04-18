3bff4ae78965cf5c448b04e3ada9c2a50b1bbb89
yongh27 <yongh9129@gmail.com>
2025-04-18 17:30:39 +0800

    更新页面0418-17:30

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // 添加Vue功能标志定义，避免警告
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
   },
-  assetsInclude: ['**/*.fbx','**/*.json'], // 确保FBX文件被正确处理
+  assetsInclude: ['**/*.fbx'], // 确保FBX文件被正确处理
   server: {
     host: true
   }
