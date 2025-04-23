b29d2bae1a32e3147f7b312fd05b78a18aa61b49
Puceqily <ack2799566@hotmail.com>
2025-04-23 23:38:37 +0800

    更新了herbal页面，进行了后端数据绑定，更新了Ancient，添加了页面跳转传递药材名称

--- frontend/src/views/Ancient.vue
+++ frontend/src/views/Ancient.vue
@@ -2,47 +2,43 @@
   <div class="Ancient">
     <div class="book-wrapper">
       <div id="flipbook">
-      <div class="hard double"><img src="/images/首页.png" alt="封面首页" /></div>
-      <div class="page"><img src="/images/兰茂.png" alt="兰茂图" /></div>
-      <div class="page"><img src="/images/介绍.png" alt="介绍图"/></div>
-      <div class="page">
-        <div class="page-bg" style="background-image: url('/images/背景.png');">
-          <img src="/images/三七.png" class="page-img" alt="三七图" @click="goToHerbal"/>
+        <div class="hard double"><img src="/images/首页.png" alt="封面首页" /></div>
+        <div class="page"><img src="/images/兰茂.png" alt="兰茂图" /></div>
+        <div class="page"><img src="/images/介绍.png" alt="介绍图"/></div>
+        <div class="page">
+          <div class="page-bg" style="background-image: url('/images/背景.png');">
+            <img src="/images/三七.png" class="page-img" alt="三七图" @click="goToHerbal('三七')" />
+          </div>
         </div>
-      </div>
-      <div class="page">
-        <div class="page-bg" style="background-image: url('/images/背景.png');">
-          <img src="/images/滇重楼.png" class="page-img" alt="滇重楼图" />
+        <div class="page">
+          <div class="page-bg" style="background-image: url('/images/背景.png');">
+            <img src="/images/滇重楼.png" class="page-img" alt="滇重楼图" @click="goToHerbal('滇重楼')" />
+          </div>
         </div>
-      </div>
-      <div class="page">
-        <div class="page-bg" style="background-image: url('/images/背景.png');">
-          <img src="/images/滇黄精.png" class="page-img" alt="滇黄精图" />
+        <div class="page">
+          <div class="page-bg" style="background-image: url('/images/背景.png');">
+            <img src="/images/滇黄精.png" class="page-img" alt="滇黄精图" @click="goToHerbal('滇黄精')" />
+          </div>
         </div>
-      </div>
-      <div class="page">
-        <div class="page-bg" style="background-image: url('/images/背景.png');">
-          <img src="/images/滇龙胆草.png" class="page-img" alt="滇龙胆图" />
+        <div class="page">
+          <div class="page-bg" style="background-image: url('/images/背景.png');">
+            <img src="/images/滇龙胆草.png" class="page-img" alt="滇龙胆图" @click="goToHerbal('滇龙胆草')" />
+          </div>
         </div>
+        <div class="hard double"><img src="/images/img_3.png" alt="滇黄精图"/></div>
       </div>
-      
-
-      <div class="hard double"><img src="/images/img_3.png" alt="滇黄精图"/></div>
-</div>
-
     </div>
   </div>
 </template>
 
 <script setup>
-
-import { onBeforeMount, } from 'vue'
+import { onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const goToHerbal = () => {
-  router.push('/herbal')
+const goToHerbal = (herbName) => {
+  router.push({ path: '/herbal', query: { herbName } })
 }
 
 onBeforeMount(() => {
@@ -73,10 +69,7 @@ onBeforeMount(() => {
   }
   document.head.appendChild(script)
 })
-
-
 </script>
-  
 
 <style scoped>
 @import './ancient.css';
