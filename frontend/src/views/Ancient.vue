a8259d48333b43824d058bf4088849d50cffb7d1
Yileen928 <Yileenbrave@gmail.com>
2025-04-23 19:35:22 +0800

    书籍十大云药

--- frontend/src/views/Ancient.vue
+++ frontend/src/views/Ancient.vue
@@ -25,9 +25,44 @@
           <img src="/images/滇龙胆草.png" class="page-img" alt="滇龙胆图" />
         </div>
       </div>
-      
 
-      <div class="hard double"><img src="/images/img_3.png" alt="滇黄精图"/></div>
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/云茯苓.png" class="page-img" alt="云茯苓图" />
+        </div>
+      </div>
+
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/天麻.png" class="page-img" alt="天麻图" />
+        </div>
+      </div>
+
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/砂仁.png" class="page-img" alt="砂仁图" />
+        </div>
+      </div>
+
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/云木香.png" class="page-img" alt="云木香图" />
+        </div>
+      </div>
+
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/铁皮石斛.png" class="page-img" alt="铁皮石斛图" />
+        </div>
+      </div>
+      
+      <div class="page">
+        <div class="page-bg" style="background-image: url('/images/背景.png');">
+          <img src="/images/灯盏花.png" class="page-img" alt="灯盏花图" />
+        </div>
+      </div>
+
+      <div class="hard double"><img src="/images/背景.png" alt="结尾" /></div>
 </div>
 
     </div>
